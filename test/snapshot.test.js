acab15e7e4d2d17b607c207006567180154d3c2b
Yannick <20306025+yandeu@users.noreply.github.com>
2020-06-02 21:39:30 +0200

    Added more snapshot tests

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -2,23 +2,37 @@ const { SnapshotInterpolation } = require('../lib/index')
 
 const SI = new SnapshotInterpolation()
 SI.interpolationBuffer.set(30) // this is only that low for testing
+const tick = 1000 / 20
 let snapshot
 let id1
 let id2
 let interpolatedSnapshot
 
+const delay = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, tick)
+  })
+}
+
 test('should be initialized', () => {
   expect(SI).not.toBeUndefined()
 })
 
-test('should create and add snapshot', done => {
-  setTimeout(() => {
-    snapshot = SI.snapshot.create([{ x: 0, y: 0 }])
-    id1 = snapshot.id
-    SI.snapshot.add(snapshot)
-    expect(snapshot).not.toBeUndefined()
-    done()
-  }, 1000 / 20)
+test('initialize with server fps', () => {
+  const SI = new SnapshotInterpolation(20)
+  const buffer = SI.interpolationBuffer.get()
+  expect(buffer).toBe(150)
+})
+
+test('should create and add snapshot', async done => {
+  await delay()
+  snapshot = SI.snapshot.create([{ x: 0, y: 0 }])
+  id1 = snapshot.id
+  SI.snapshot.add(snapshot)
+  expect(snapshot).not.toBeUndefined()
+  done()
 })
 
 test('snapshot id should be 6 chars long', () => {
@@ -34,14 +48,19 @@ test('getting latest snapshot should have same id', () => {
   expect(s.id).toBe(snapshot.id)
 })
 
-test('should create and add another snapshot', done => {
-  setTimeout(() => {
-    snapshot = SI.snapshot.create([{ x: 10, y: 10 }])
-    id2 = snapshot.id
-    SI.snapshot.add(snapshot)
-    expect(SI.vault.size).toBe(2)
-    done()
-  }, 1000 / 20)
+test('worldState should be an array', () => {
+  expect(() => {
+    SI.snapshot.create({ x: 10, y: 10 })
+  }).toThrow()
+})
+
+test('should create and add another snapshot', async done => {
+  await delay()
+  snapshot = SI.snapshot.create([{ x: 10, y: 10 }])
+  id2 = snapshot.id
+  SI.snapshot.add(snapshot)
+  expect(SI.vault.size).toBe(2)
+  done()
 })
 
 test('should get interpolated value', () => {
@@ -59,4 +78,9 @@ test('values should be interpolated', () => {
   interpolatedSnapshot.state.forEach(entity => {
     expect(entity.x > 0 && entity.x < 10).toBeTruthy()
   })
+})
+
+test('timeOffset should >= 0', () => {
+  const timeOffset = SI.timeOffset
+  expect(timeOffset >= 0).toBeTruthy()
 })
